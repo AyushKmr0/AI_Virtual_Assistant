a74aae9631e205233297286bc85186ad8498a3c7
Ayush kumar <165952944+AyushKmr0@users.noreply.github.com>
2025-07-02 00:26:08 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -30,8 +30,8 @@ export const signUp = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "None",
+            secure: true,
         });
 
         return res.status(201).json(user);
@@ -61,8 +61,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "None",
+            secure: true,
         });
 
         return res.status(200).json(user);
@@ -90,8 +90,8 @@ export const handleSocialLogin = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "None",
+            secure: true,
         });
 
         res.redirect("http://localhost:5173/oauth-success");
